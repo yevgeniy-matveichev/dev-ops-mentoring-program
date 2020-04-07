f636622c2bf93608296ddd8ba869a3cb1ff79611
Yevgeniy Matveichev <yevgeniy_matveichev@epam.com>
2020-04-07 14:05:23 +0300

    an empty project is created for module 1

---
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module1.TypesAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
