c96dc37829a0439a7514bf28fa6d63e52d81e45e
Yevgeniy Matveichev <yevgeniy_matveichev@epam.com>
2020-04-10 12:17:23 +0300

    Task 1: shapes

---
+++ Module1.TypesAndClasses/Module1.TypesAndClasses.Tests/UnitTest1.cs
@@ -0,0 +1,70 @@
+using Module1.TypesAndClasses.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Module1.TypesAndClasses.Tests
+{
+    public class TestClasses : IDisposable
+    {
+        #region private fields
+
+        private readonly Mock<IShape> _circle;
+        private readonly Mock<IShape> _ellipse;
+        private readonly Mock<IShape> _equilateralTriangle;
+        private readonly Mock<IShape> _rectangle;
+        private readonly Mock<IShape> _regularPolygon;
+
+        #endregion
+
+        #region constructor, disposing
+
+        public TestClasses()
+        {
+            _circle = new Mock<IShape>();
+            _regularPolygon = new Mock<IShape>();
+            _equilateralTriangle = new Mock<IShape>();
+
+            // todo: remove after the methods are implemented
+            _circle.Setup(c => c.Perimeter()).Returns(10);
+            _circle.Setup(c => c.Square()).Returns(100);
+
+            _regularPolygon.Setup(c => c.Perimeter()).Returns(86);
+            _regularPolygon.Setup(c => c.Square()).Returns(15);
+
+            _equilateralTriangle.Setup(c => c.Perimeter()).Returns(10);
+            _equilateralTriangle.Setup(c => c.Square()).Returns(100);
+        }
+
+        public void Dispose()
+        {
+            _circle.Reset();
+        }
+
+        #endregion
+
+        [Fact]
+        public void TestShapesToString()
+        {
+            var shapes = new List<IShape>
+            {
+                _circle.Object,
+                // todo: add all other shapes
+            };
+
+            foreach(var shape in shapes)
+            {
+                Assert.Equal($"Shape: '{shape.GetType().Name}'. Square = {shape.Square()}, perimeter = {shape.Perimeter()}", shape.ToString());
+            }
+        }
+
+        [Fact]
+        public void TestShapesEquals()
+        {            
+            Assert.NotEqual(_equilateralTriangle.Object, _regularPolygon.Object);
+            Assert.NotEqual(_circle.Object, _regularPolygon.Object);
+            Assert.Equal(_equilateralTriangle.Object, _circle.Object);
+        }
+    }
+}
--- Module1.TypesAndClasses/Module1.TypesAndClasses/Program.cs
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Module1.TypesAndClasses.Interfaces;
+using Module1.TypesAndClasses.Shapes;
+using System;
+using System.Collections.Generic;
 
 namespace Module1.TypesAndClasses
 {
@@ -6,7 +9,21 @@ namespace Module1.TypesAndClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Module 1. Shapes example.");
+
+            var shapes = new List<IShape>
+            {
+                new Circle(),
+                new Ellipse(),
+                new EquilateralTriangle(),
+                new Rectangle(),
+                new RegularPolygon()
+            };
+
+            foreach(var shape in shapes)
+            {
+                Console.WriteLine(shape.ToString());
+            }
         }
     }
 }
