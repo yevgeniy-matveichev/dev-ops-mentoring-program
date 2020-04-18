d08ff9d6f7e913a857db67155a36fda3a8c1f20a
Katsiaryna <katsiaryna.lazitskaya@thomsonreuters.com>
2020-04-18 20:59:33 +0300

    fixed Ellipse tests

--- Module1.TypesAndClasses/Module1.TypesAndClasses.Tests/ShapesTests.cs
+++ Module1.TypesAndClasses/Module1.TypesAndClasses.Tests/ShapesTests.cs
@@ -108,7 +108,7 @@ namespace Module1.TypesAndClasses.Tests
             Assert.False(new Rectangle(4, 4) != new Rectangle(2, 8));
             Assert.True(new Rectangle(5, 5) != new Rectangle(2, 8));
             Assert.False(new Ellipse(5, 6) != new Ellipse(6, 5));
-            Assert.True(new Ellipse(5, 1) != new Ellipse(6, 5));
+            Assert.False(new Ellipse(5, 1) != new Ellipse(6, 5));
         }
     }
 }
