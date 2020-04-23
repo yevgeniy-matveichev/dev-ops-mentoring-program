ff6c5e295936ce3b9b7b138111426803d24302d8
Katsiaryna <katsiaryna.lazitskaya@thomsonreuters.com>
2020-04-23 18:40:46 +0300

    removed commented code

--- Module1.TypesAndClasses/Module1.TypesAndClasses/Generics/ShapePrinter.cs
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Generics/ShapePrinter.cs
@@ -1,5 +1,4 @@
 ﻿using Module1.TypesAndClasses.Interfaces;
-using Module1.TypesAndClasses.Shapes;
 using System;
 
 namespace Module1.TypesAndClasses.Generics
@@ -10,40 +9,5 @@ namespace Module1.TypesAndClasses.Generics
         {
             Console.WriteLine(shape.ToString());
         }
-
-        //public override string ToString()
-        //{
-        //    //var eTriangle = new EquilateralTriangle(5);
-
-        //    //var sb = new System.Text.StringBuilder();
-        //    //sb.Append($"Shape: '{nameof(EquilateralTriangle)}'. ");
-        //    //sb.Append($"Square = {eTriangle.Square()}, ");
-        //    //sb.Append($"perimeter = {eTriangle.Perimeter()}. ");
-        //    //sb.Append($"Side = {_a}");
-
-        //    //return sb.ToString();
-
-        //    return $"Shape: '{shape.GetType().Name}'. Square = {shape.Square()}, perimeter = {shape.Perimeter()}"
-        //}
-
-        //public bool PerimeterEquals<T>(T shape1, T shape2)
-        //{
-        //    if (shape1 == null)
-        //    {
-        //        if (shape2 == null)
-        //        {
-        //            throw new ArgumentNullException("Objects shape1 and shape2 were null.");
-        //        }
-
-        //        throw new ArgumentNullException(nameof(shape1));
-        //    }
-
-        //    if (shape2 == null)
-        //    {
-        //        throw new ArgumentNullException(nameof(shape2));
-        //    }
-
-        //    return shape1.Perimeter() == shape2.Perimeter();
-        //}
     }
 }
