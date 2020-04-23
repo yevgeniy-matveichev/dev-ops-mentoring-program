bdebf3f7f2ab6f71a68cf78dbec1a9e3bf39d272
Siarhei Malyshau <siarhei_malyshau@epam.com>
2020-04-23 14:37:44 +0300

    Rectangle

--- Module1.TypesAndClasses/Module1.TypesAndClasses/Shapes/Rectangle.cs
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Shapes/Rectangle.cs
@@ -3,65 +3,50 @@ using System;
 
 namespace Module1.TypesAndClasses.Shapes
 {
-    public class Rectangle : IShape
+    public class Rectangle : BaseShape 
     {
         #region  private fields
 
         private readonly int _sideA;
         private readonly int _sideB;
+        private readonly Units _metricName;
+        
 
-        #endregion 
+        #endregion
 
-        public Rectangle(int a, int b)
+       new string shapeName = nameof(Rectangle);
+              
+        public Rectangle(int a, int b, Units units) : base(units)
         {
             if (a >= 0 && b >= 0)
             {
                 _sideA = a;
                 _sideB = b;
+                _metricName = units;
             }
             else 
             { 
                 throw new ArgumentException($"Cannot perform an operation: Argument values must be positive "); 
             }
         }
-
+               
         #region Public Functions
               
-        public int Perimeter()
+        public override double Perimeter()
         {
             return (_sideA + _sideB) * 2;
         }
 
-        public long Square()
+        public override double Square()
         {
             return (_sideA * _sideB);
         }
         #endregion
-
-        #region Public Methods
+         #region Public Methods
         public override string ToString()
         {
-            return $"Shape: '{nameof(Rectangle)}'. Square = {Square()}, perimeter = {Perimeter()}";
+            return $"Shape: '{nameof(Rectangle)}'. Square = {Square()} {_metricName}2, perimeter = {Perimeter()} {_metricName}, SideA = {_sideA}, SideB = {_sideB}";
         }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null)
-                return false;
-            Rectangle m = obj as Rectangle;
-            return m.Perimeter() == this.Perimeter();
-        }
-        
-        public static bool operator ==(Rectangle obj1, Rectangle obj2)
-        {
-            return obj1.Square() == obj2.Square();
-        }
-       
-        public static bool operator !=(Rectangle obj1, Rectangle obj2)
-        {
-            return obj1.Square() != obj2.Square();
-        }
-
         #endregion
-    }
+}
 }
