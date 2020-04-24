43a68b9d9bae420b440c3e83403e876f3117fada
Katsiaryna <katsiaryna.lazitskaya@thomsonreuters.com>
2020-04-24 13:26:20 +0300

    updated ShapePrinter.cs

--- Module1.TypesAndClasses/Module1.TypesAndClasses/Generics/ShapePrinter.cs
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Generics/ShapePrinter.cs
@@ -9,5 +9,35 @@ namespace Module1.TypesAndClasses.Generics
         {
             Console.WriteLine(shape.ToString());
         }
+
+        public bool PerimeterEquals(T shape, T other)
+        {
+            if (shape == null)
+            {
+                throw new ArgumentNullException(nameof(shape));
+            }
+
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            return shape.Perimeter() == other.Perimeter();
+        }
+
+        public bool SquareEquals(T shape, T other)
+        {
+            if (shape == null)
+            {
+                throw new ArgumentNullException(nameof(shape));
+            }
+
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            return shape.Square() == other.Square();
+        }
     }
 }
