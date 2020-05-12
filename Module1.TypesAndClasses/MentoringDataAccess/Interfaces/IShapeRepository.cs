6c63147dadf680f7d956ecb188c815b605c525c8
Usagich <liloddy@gmail.com>
2020-05-13 01:20:06 +0300

    added data layer

---
+++ Module1.TypesAndClasses/MentoringDataAccess/Interfaces/IShapeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MentoringDataAccess.Interfaces
+{
+    public interface IShapeRepository
+    {
+
+        public string ReadShape(string shapeName);
+
+    }
+}
