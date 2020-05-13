b895e27b96a0ddad7095eeac96960acc7a9ddd91
Katsiaryna <katsiaryna.lazitskaya@thomsonreuters.com>
2020-05-13 21:30:41 +0300

    updated Program.cs

--- Module1.TypesAndClasses/Module1.TypesAndClasses/Program.cs
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Mentoring.Shapes.Interfaces;
 using Mentoring.Shapes.Shapes;
 
@@ -6,53 +7,74 @@ namespace Module1.TypesAndClasses
 {
     class Program
     {
+        static readonly Dictionary<string, string> supportedShapes = new Dictionary<string, string>
+            {
+                { "c", "Circle" },
+                { "e", "Ellipse" },
+                { "r", "Rectangle" },
+                { "t", "Triangle" }
+            };
+
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello! This is Shapes program. Supported shapes: Circle, Ellipse, Rectangle and Triangle.\n");
-            Console.WriteLine($"Please enter one of the options bellow: " +
-                              $"\n1. '{args[0]}' to process Circle " +
-                              $"\n2. '{args[1]}' to process Ellipse " +
-                              $"\n3. '{args[2]}' to process Rectangle " +
-                              $"\n4. '{args[3]}' to process Triangle " +
-                              $"\n5. Empty to process all the shapes\n");
-
-            for (var attempt = 0; attempt < 4; attempt++)
+            if (args.Length == 0)
             {
-                string userInput = Console.ReadLine();
+                Console.WriteLine($"Hello! This is Shapes program. Supported shapes: {supportedShapes["c"]}, {supportedShapes["e"]}, {supportedShapes["r"]}, {supportedShapes["t"]}\n");
 
-                if (attempt == 3)
-                {
-                    Console.WriteLine("\nYou've reached the limit of attempts :(");
-                    break;
-                }
+            } else
+            {
+                Console.WriteLine($"Hello! This is Shapes program. Supported shapes: {string.Join(", ", args)}");
+                Console.WriteLine("Please enter the shape:\n");
 
-                switch (userInput)
+                for (var attempt = 0; attempt < 5; attempt++)
                 {
-                    case "c":
-                        // todo: read the shape of type Circle from the ShapesService
-                        Console.WriteLine("Processing Circle...");
-                        return;
-                    case "e":
-                        // todo: read the shape of type Ellipse from the ShapesService
-                        Console.WriteLine("Processing Ellipse...");
-                        return;
-                    case "r":
-                        // todo: read the shape of type Rectangle from the ShapesService
-                        Console.WriteLine("Processing Rectangle...");
-                        return;
-                    case "t":
-                        // todo: read the shape of type EquilateralTriangle from the ShapesService
-                        Console.WriteLine("Processing Triangle...");
-                        return;
-                    case "":
-                        // todo: read all shapes from the ShapesService
-                        Console.WriteLine("Processing all shapes...");
-                        return;
-                    default:
-                        Console.WriteLine($"The shape is not supported. Supported shapes: {args[0]} - Circle, {args[1]} - Ellipse, {args[2]} - Rectangle, {args[3]} - Triangle");
+                    string userInput = Console.ReadLine();
+
+                    if (attempt == 3)
+                    {
+                        Console.WriteLine("\nYou've reached the limit of attempts :(");
                         break;
+                    }
+
+                    switch (userInput)
+                    {
+                        case "c":
+                            // todo: read the shape of type Circle from the ShapesService
+                            Console.WriteLine("Processing Circle...");
+                            return;
+                        case "e":
+                            // todo: read the shape of type Ellipse from the ShapesService
+                            Console.WriteLine("Processing Ellipse...");
+                            return;
+                        case "r":
+                            // todo: read the shape of type Rectangle from the ShapesService
+                            Console.WriteLine("Processing Rectangle...");
+                            return;
+                        case "t":
+                            // todo: read the shape of type EquilateralTriangle from the ShapesService
+                            Console.WriteLine("Processing Triangle...");
+                            return;
+                        case "":
+                            // todo: read all shapes from the ShapesService
+                            Console.WriteLine("Processing all shapes...");
+                            return;
+                        default:
+                            Console.WriteLine($"The shape is not supported. Supported shapes: {string.Join(", ", args)}");
+                            break;
+                    }
                 }
             }
+
+            //Console.WriteLine($"Please enter one of the options bellow: " +
+            //                  $"\n1. '{args[0]}' to process Circle " +
+            //                  $"\n2. '{args[1]}' to process Ellipse " +
+            //                  $"\n3. '{args[2]}' to process Rectangle " +
+            //                  $"\n4. '{args[3]}' to process Triangle ");
+        }
+
+        private string ArgsToShapeName(string[] args)
+        {
+            return "";
         }
     }
 }
