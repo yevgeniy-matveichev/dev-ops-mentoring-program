bac11d3f9a8a2c011397c3c76f89fc7c46e4404e
Siarhei Malyshau <siarhei_malyshau@epam.com>
2020-05-13 09:35:30 +0300

    Service Layer

---
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Services/IShapesService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mentoring.Shapes.Interfaces;
+
+namespace Module1.TypesAndClasses.Services
+{
+   public interface IShapesService
+    {
+        string ReadSHape(ShapeType shapetype);
+        string IShape();
+    }
+}
---
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Services/ShapesService.cs
@@ -0,0 +1,28 @@
+﻿using Mentoring.Shapes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Mentoring.Shapes;
+
+namespace Module1.TypesAndClasses.Services
+{
+    class ShapesService : IShapesService
+    {
+        #region  private fields
+
+        private ShapeType _shapeType;
+        
+        #endregion
+
+        public string IShape()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadSHape(ShapeType shapetype)
+        {
+            _shapeType = shapetype;
+            return $"{_shapeType}.json";
+        }
+    }
+}
