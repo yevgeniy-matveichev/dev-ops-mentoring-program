f2a21c71190c28e41b06c872237cd7440b6b8391
Yevgeniy Matveichev <yevgeniy_matveichev@epam.com>
2020-05-14 23:20:01 +0300

    added a failing test for units calculation

---
+++ Module1.TypesAndClasses/Module1.TypesAndClasses.Tests/Shapes/ShapeCalculationTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Module1.TypesAndClasses.Tests.Shapes
+{
+    public class ShapeCalculationTest
+    {
+        [Fact]
+        public void TestMetrics()
+        {
+
+        }
+    }
+}
