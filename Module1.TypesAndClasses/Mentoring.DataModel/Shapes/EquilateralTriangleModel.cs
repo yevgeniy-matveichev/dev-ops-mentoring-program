05cccdeeb7942d8db23547621dc72112986a290b
Katsiaryna <katsiaryna.lazitskaya@thomsonreuters.com>
2020-05-15 16:14:44 +0300

    added Model file for triangle

---
+++ Module1.TypesAndClasses/Mentoring.DataModel/Shapes/EquilateralTriangleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mentoring.DataModel.Shapes
+{
+    class EquilateralTriangleModel
+    {
+        public double SideInMeters { get; set; }
+        public string Unit { get; set; }
+    }
+}
