27cbf0ab180da5d52ab79b1910fc7d47d92fe1db
Siarhei Malyshau <siarhei_malyshau@epam.com>
2020-06-04 19:07:12 +0300

    small fixes

--- Module1.TypesAndClasses/Module1.TypesAndClasses/Services/ShapesService.cs
+++ Module1.TypesAndClasses/Module1.TypesAndClasses/Services/ShapesService.cs
@@ -31,26 +31,22 @@ namespace Module1.TypesAndClasses.Services
             {
                 case ShapeTypes.EquilateralTriangle:
                     {
-                        ShapesRepository<EquilateralTriangleModel> reader = new ShapesRepository<EquilateralTriangleModel>();
-                        EquilateralTriangleModel triangleModel = reader.ReadShape(shapeName);
+                        EquilateralTriangleModel triangleModel = new ShapesRepository<EquilateralTriangleModel>().ReadShape(shapeName);
                         return new EquilateralTriangle(triangleModel.Side, triangleModel.Unit);
                     }
                 case ShapeTypes.Circle: 
                     {
-                        ShapesRepository<CircleModel> reader = new ShapesRepository<CircleModel>();
-                        CircleModel circleModel = reader.ReadShape(shapeName);
+                        CircleModel circleModel = new ShapesRepository<CircleModel>().ReadShape(shapeName);
                         return new Circle(circleModel.Radius, circleModel.Unit);
                     }
                 case ShapeTypes.Rectangle:
                     {
-                        ShapesRepository<RectangleModel> reader = new ShapesRepository<RectangleModel>();
-                        RectangleModel rectangleModel = reader.ReadShape(shapeName);
+                        RectangleModel rectangleModel = new ShapesRepository<RectangleModel>().ReadShape(shapeName);
                         return new Rectangle(rectangleModel.SideA, rectangleModel.SideB, rectangleModel.Unit);
                     }
                 case ShapeTypes.Ellipse:
                     {
-                        ShapesRepository<EllipseModel> reader = new ShapesRepository<EllipseModel>();
-                        EllipseModel ellipseModel = reader.ReadShape(shapeName);
+                        EllipseModel ellipseModel = new ShapesRepository<EllipseModel>().ReadShape(shapeName);
                         return new Ellipse(ellipseModel.Radius1, ellipseModel.Radius2, ellipseModel.Unit);
                     }
                 default:
