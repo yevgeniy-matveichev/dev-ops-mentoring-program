934cf56f36c5ca4c318b937d2c4dfbc67728358d
Katsiaryna <katsiaryna.lazitskaya@thomsonreuters.com>
2020-06-16 00:24:07 +0300

    added tests in CommandsFactoryTests.cs

--- Module1.TypesAndClasses/Module1.TypesAndClasses.Tests/Factories/CommandsFactoryTests.cs
+++ Module1.TypesAndClasses/Module1.TypesAndClasses.Tests/Factories/CommandsFactoryTests.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Module1.TypesAndClasses.Commands;
+using Module1.TypesAndClasses.Factories;
+using System;
+using Xunit;
 
 namespace Module1.TypesAndClasses.Tests.Factories
 {
-    class CommandsFactoryTests
+    public class CommandsFactoryTests
     {
+        private readonly CommandsFactory _factory = new CommandsFactory(new string[] { "param1", "param2", "param3", "param4" });
+
+        [Theory]
+        [InlineData("list", typeof(ListCommand))]
+        [InlineData("help", typeof(HelpCommand))]
+        [InlineData("export", typeof(ExportCommand))]
+        [InlineData("import", typeof(ImportCommand))]
+        public void TestCommandsFactory_Create(string param, Type t)
+        {
+            IInputCommand command = _factory.Create(param);
+            Assert.NotNull(command);
+            Assert.IsType(t, command);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("wrongCommand")]
+        public void TestCommandsFactory_Fail(string param)
+        {
+            Assert.Throws<NotSupportedException>(() => _factory.Create(param)); 
+        }
     }
 }
